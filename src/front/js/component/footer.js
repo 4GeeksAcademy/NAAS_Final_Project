9e54414eff03ddb3b6f079deb4385f68b9d5f549
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2024-01-22 21:14:39 +0000

    Footer al 90%

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3 text-center bg-dark text-white">
+		<div class="container text-center">
+			<div class="row p-4">
+				<div class="col-4 text-start">
+					<h4><i class="pe-2 fa-solid fa-house"></i>SNAPIFY</h4>
+					<p> Únete a nuestra comunidad </p>
+					<h4>
+						<i class="pe-2 fa-brands fa-discord"></i>
+						<i class="pe-2 fa-brands fa-youtube"></i>
+						<i class="pe-2 fa-brands fa-twitter"></i>
+						<i class="pe-2 fa-brands fa-instagram"></i>
+					</h4>
+				</div>
+				<div class="text-start col-3">
+					<h4>Explorar</h4>
+					<nav class="nav flex-column">
+						<a class="p-0 fs-5 text-white nav-link" href="#">Galería</a>
+						<a class="p-0 fs-5 text-white nav-link" href="#">Rankings</a>
+						<a class="p-0 fs-5 text-white nav-link" href="#">Registrarse</a>
+					</nav>
+				</div>
+				<div class="text-start col-5">
+					<h4>Únete A Nuestro Boletín Semanal</h4>
+					<p> Obtenga promociones y actualizaciones exclusivas directamente en su bandeja de entrada. </p>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Enter your email here" aria-label="Enter your email here">
+						</input>
+						<button type="button" class=" px-5 btn btn-success"><i class="pe-2 fa-regular fa-envelope"></i>Suscribe</button>
+					</div>
+				</div>
+			</div>
+		</div>	
 	</footer>
 );
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,6 +11,8 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
+import "../styles/home.css";
+
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
