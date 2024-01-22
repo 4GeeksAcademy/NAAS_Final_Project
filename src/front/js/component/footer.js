e294e6b1d3d384b42dd6ad159c3c73206f9c0185
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2024-01-22 22:17:18 +0000

    Footer 99%

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,37 +1,48 @@
 import React, { Component } from "react";
+import "../../styles/background.css";
+import rigoImageUrl from "../../img/logo1.jpeg";
+import { Link } from 'react-router-dom';
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center bg-dark text-white">
-		<div class="container text-center">
-			<div class="row p-4">
-				<div class="col-4 text-start">
-					<h4><i class="pe-2 fa-solid fa-house"></i>SNAPIFY</h4>
+	<footer className="footer mt-auto py-3 text-center color-back2 text-white text">
+		<div className="container text-center">
+			<div className="row p-4">
+				<div className="col-4 text-start">
+					<h4>
+						<img
+							src={rigoImageUrl}
+							alt="logo"
+							style={{ width: '40px', borderRadius: '100%', marginRight: '10px' }}
+						/>
+						SNAPIFY
+					</h4>
+
 					<p> Únete a nuestra comunidad </p>
 					<h4>
-						<i class="pe-2 fa-brands fa-discord"></i>
-						<i class="pe-2 fa-brands fa-youtube"></i>
-						<i class="pe-2 fa-brands fa-twitter"></i>
-						<i class="pe-2 fa-brands fa-instagram"></i>
+						<i className="color-icon pe-2 fa-brands fa-discord"></i>
+						<i className="color-icon pe-2 fa-brands fa-youtube"></i>
+						<i className="color-icon pe-2 fa-brands fa-twitter"></i>
+						<i className="color-icon pe-2 fa-brands fa-instagram"></i>
 					</h4>
 				</div>
-				<div class="text-start col-3">
+				<div className="text-start col-3">
 					<h4>Explorar</h4>
-					<nav class="nav flex-column">
-						<a class="p-0 fs-5 text-white nav-link" href="#">Galería</a>
-						<a class="p-0 fs-5 text-white nav-link" href="#">Rankings</a>
-						<a class="p-0 fs-5 text-white nav-link" href="#">Registrarse</a>
+					<nav className="nav flex-column">
+						<a className="p-0 fs-5 text-white nav-link" href="#">Galería</a>
+						<a className="p-0 fs-5 text-white nav-link" href="#">Rankings</a>
+						<a className="p-0 fs-5 text-white nav-link" href="#">Registrarse</a>
 					</nav>
 				</div>
-				<div class="text-start col-5">
+				<div className="text-start col-5">
 					<h4>Únete A Nuestro Boletín Semanal</h4>
 					<p> Obtenga promociones y actualizaciones exclusivas directamente en su bandeja de entrada. </p>
-					<div class="input-group mb-3">
-						<input type="text" class="form-control" placeholder="Enter your email here" aria-label="Enter your email here">
+					<div className="input-group mb-3">
+						<input type="text" className="form-control" placeholder="Enter your email here" aria-label="Enter your email here">
 						</input>
-						<button type="button" class=" px-5 btn btn-success"><i class="pe-2 fa-regular fa-envelope"></i>Suscribe</button>
+						<button type="button" className=" px-5 btn color-call color-text"><i className="pe-2 fa-regular fa-envelope"></i>Suscribe</button>
 					</div>
 				</div>
 			</div>
-		</div>	
+		</div>
 	</footer>
 );
