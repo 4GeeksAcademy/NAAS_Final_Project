899f194bdc7acbbf1c77a087bf3a77b3fc85e582
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2024-01-31 22:16:21 +0000

    Finalizado estilos de logros

--- src/front/js/component/logros.js
+++ src/front/js/component/logros.js
@@ -28,28 +28,28 @@ export const Logros = () => {
               <h5>Nuevo Usuario</h5>
               <img src="https://i.imgur.com/3mUKHYV.png" alt="new user of Snapify" className="achievement " />
               {hoverAchievement === 'Nuevo Usuario' && (
-                <div className="achievement-text">Otorgado al registrarse.</div>
+                <div className="achievement-text">Bienvenido a Snapify. <em>Otorgado al registrarse.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('Primer SNAP!')} onMouseLeave={() => handleMouseLeave}>
               <h5>Primer SNAP!</h5>
               <img src="https://i.imgur.com/fIScyew.png" alt="first snap" className="achievement" />
               {hoverAchievement === 'Primer SNAP!' && (
-                <div className="achievement-text">Da el primer paso y envía tu debut fotográfico al concurso.</div>
+                <div className="achievement-text">Da el primer paso. <em>Otorgado al debutar en tu primer concurso.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('Corazón Emergente')} onMouseLeave={() => handleMouseLeave}>
               <h5>Corazón Emergente</h5>
               <img src="https://i.imgur.com/iDS1yFR.png" alt="shining heart" className="achievement" />
               {hoverAchievement === 'Corazón Emergente' && (
-                <div className="achievement-text">Atrae miradas, consigue 10 likes en una sola foto.</div>
+                <div className="achievement-text">Atrae miradas. <em>Otorgado al conseguir 10 "likes" en una sola foto.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('Influencer Novato')} onMouseLeave={() => handleMouseLeave}>
               <h5>Influencer Novato</h5>
               <img src="https://i.imgur.com/dLIS0G8.png" alt="follow" className="achievement" />
               {hoverAchievement === 'Influencer Novato' && (
-                <div className="achievement-text">Bienvenido a la vida pública, acumula 20 seguidores en tu perfil.</div>
+                <div className="achievement-text">Bienvenido a la vida pública. <em>Otorgado al acumular 20 seguidores en tu perfil.</em></div>
               )}
             </div>
           </div>
@@ -61,21 +61,21 @@ export const Logros = () => {
               <h5>Ingeniero de la Imagen</h5>
               <img src="https://i.imgur.com/Xi9pu83.png" alt="perspective" className="achievement" />
               {hoverAchievement === 'Ingeniero de la Imagen' && (
-                <div className="achievement-text">Experimenta con tu arte, envía tus fotos a 10 concursos.</div>
+                <div className="achievement-text">Experimenta con tu arte. <em>Otorgado al enviar tus fotos a 10 concursos.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('Ganador de Corazones')} onMouseLeave={() => handleMouseLeave}>
               <h5>Ganador de Corazones</h5>
               <img src="https://i.imgur.com/qKXtxv2.png" alt="several hearts" className="achievement" />
               {hoverAchievement === 'Ganador de Corazones' && (
-                <div className="achievement-text">Conquista corazones, alcanza los 50 likes en una única imagen.</div>
+                <div className="achievement-text">Conquista corazones. <em>Otorgado al obtener 50 "likes" en una única foto.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('Embajador')} onMouseLeave={() => handleMouseLeave}>
               <h5>Embajador Fotográfico</h5>
               <img src="https://i.imgur.com/shAk6a4.png" alt="ambassador" className="achievement" />
               {hoverAchievement === 'Embajador' && (
-                <div className="achievement-text">Lleva tu influencia más allá, alcanza la impresionante cifra de 50 seguidores.</div>
+                <div className="achievement-text">Lleva tu influencia más allá. <em>Otorgado al alcanzar la impresionante cifra de 50 seguidores.</em></div>
               )}
             </div>
           </div>
@@ -87,21 +87,21 @@ export const Logros = () => {
               <h5>Rey de Corazones</h5>
               <img src="https://i.imgur.com/g41w1dC.png" alt="king of hearts" className="achievement" />
               {hoverAchievement === 'King' && (
-                <div className="achievement-text">Escala la montaña de likes, obtén 100 likes en una sola foto.</div>
+                <div className="achievement-text">Corónate. <em>Otorgado al conseguir 100 "likes" en una sola foto.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('legion')} onMouseLeave={() => handleMouseLeave}>
               <h5>Lider de la Legión</h5>
               <img src="https://i.imgur.com/XmSFMDv.png" alt="group of people" className="achievement" />
               {hoverAchievement === 'legion' && (
-                <div className="achievement-text">Conduce la marcha, alcanza la impresionante cifra de 100 seguidores.</div>
+                <div className="achievement-text">Conduce la marcha. <em>Otorgado al alcanzar la impresionante cifra de 100 seguidores.</em></div>
               )}
             </div>
             <div className="achievement-title" onMouseEnter={() => handleMouseEnter('pioneer')} onMouseLeave={() => handleMouseLeave}>
               <h5>Pionero</h5>
               <img src="https://i.imgur.com/VZdxqYf.png" alt="star" className="achievement" />
               {hoverAchievement === 'pioneer' && (
-                <div className="achievement-text">Sé un ejemplo a seguir, participa en 30 concursos con consistencia y pasión.</div>
+                <div className="achievement-text">Sé un ejemplo a seguir. <em>Otorgado al participar en 30 concursos con consistencia y pasión.</em></div>
               )}
             </div>
           </div>
