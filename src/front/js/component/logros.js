b685c7f70fb11bc064697c885bd04a3aa1a4f782
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2024-01-31 15:16:06 +0000

    Adding titles

--- src/front/js/component/logros.js
+++ src/front/js/component/logros.js
@@ -11,23 +11,60 @@ export const Logros = () => {
       {/* Contenido de la sección de logros */}
       <h3 className="color-text d-flex justify-content-center">Logros</h3>
       <div className="bg-dark color-text d-flex flex-column align-items-center">
-        <div className="d-flex justify-content-center">
-          <img src="https://i.imgur.com/3mUKHYV.png" alt="new user of Snapify" className="first-achievement " />
-          <img src="https://i.imgur.com/fIScyew.png" alt="first snap" className="second-achievement" />
-          <img src="https://i.imgur.com/iDS1yFR.png" alt="shining heart" className="third-achievement" />
-          <img src="https://i.imgur.com/dLIS0G8.png" alt="follow" className="fourth-achievement" />
+        <div className="tier-container">
+          <h4>Tier Iniciado</h4>
+          <div className="d-flex justify-content-center">
+            <div className="achievement-title">
+              <h5>Nuevo Usuario</h5>
+              <img src="https://i.imgur.com/3mUKHYV.png" alt="new user of Snapify" className="achievement " />
+            </div>
+            <div className="achievement-title">
+              <h5>Primer SNAP!</h5>
+              <img src="https://i.imgur.com/fIScyew.png" alt="first snap" className="achievement" />
+            </div>
+            <div className="achievement-title">
+              <h5>Corazón Emergente</h5>
+              <img src="https://i.imgur.com/iDS1yFR.png" alt="shining heart" className="achievement" />
+            </div>
+            <div className="achievement-title">
+              <h5>Influencer Novato</h5>
+              <img src="https://i.imgur.com/dLIS0G8.png" alt="follow" className="achievement" />
+            </div>
+          </div>
         </div>
-        &nbsp;
-        <div className="d-flex justify-content-center">
-          <img src="https://i.imgur.com/Xi9pu83.png" alt="perspective" className="fifth-achievement" />
-          <img src="https://i.imgur.com/qKXtxv2.png" alt="several hearts" className="sixth-achievement" />
-          <img src="https://i.imgur.com/shAk6a4.png" alt="ambassador" className="seventh-achievement" />
+        <div className="tier-container">
+          <h4>Tier Intermedio</h4>
+          <div className="d-flex justify-content-center">
+            <div className="achievement-title">
+              <h5>Ingeniero de la Imagen</h5>
+              <img src="https://i.imgur.com/Xi9pu83.png" alt="perspective" className="achievement" />
+            </div>
+            <div className="achievement-title">
+              <h5>Ganador de Corazones</h5>
+              <img src="https://i.imgur.com/qKXtxv2.png" alt="several hearts" className="achievement" />
+            </div>
+            <div className="achievement-title">
+              <h5>Embajador Fotográfico</h5>
+              <img src="https://i.imgur.com/shAk6a4.png" alt="ambassador" className="achievement" />
+            </div>
+          </div>
         </div>
-        &nbsp;
-        <div className="d-flex justify-content-center">
-          <img src="https://i.imgur.com/g41w1dC.png" alt="king of hearts" className="eighth-achievement" />
-          <img src="https://i.imgur.com/XmSFMDv.png" alt="group of people" className="ninth-achievement" />
-          <img src="https://i.imgur.com/VZdxqYf.png" alt="star" className="tenth-achievement" />
+        <div className="tier-container">
+          <h4>Tier Avanzado</h4>
+          <div className="d-flex justify-content-center">
+            <div className="achievement-title">
+              <h5>Rey de Corazones</h5>
+              <img src="https://i.imgur.com/g41w1dC.png" alt="king of hearts" className="achievement" />
+            </div>
+            <div className="achievement-title">
+              <h5>Lider de la Legión</h5>
+              <img src="https://i.imgur.com/XmSFMDv.png" alt="group of people" className="achievement" />
+            </div>
+            <div className="achievement-title">
+              <h5>Pionero</h5>
+              <img src="https://i.imgur.com/VZdxqYf.png" alt="star" className="achievement" />
+            </div>
+          </div>
         </div>
       </div>
       {/* ...otros elementos de logros */}
