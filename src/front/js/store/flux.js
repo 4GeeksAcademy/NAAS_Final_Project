81d336bb98186cf6a3c7f3cb2cf54cecaf92a2ee
AgustinaGonzalezAG <139406447+AgustinaGonzalezAG@users.noreply.github.com>
2024-02-04 00:13:41 +0000

    [My local changes before the pull]

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,7 +6,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			userRankings: [
 				{ rank: 1, imageUrl: 'URL_DEL_USUARIO_1', username: 'Usuario 1', numbers: [456, 789, 123] },
 				{ rank: 2, imageUrl: 'URL_DEL_USUARIO_2', username: 'Usuario 2', numbers: [789, 456, 123] },
-			  ],
+			],
 			photoRankings: [
 				{ rank: 1, imageUrl: 'URL_DEL_USUARIO_1', username: 'nombre 1', numbers: [45, 7, 12] },
 				{ rank: 1, imageUrl: 'URL_DEL_USUARIO_1', username: 'nombre 2', numbers: [3, 2, 62] },
@@ -22,6 +22,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			events: [],
 			currentEvent: [],
 			statusActive: false,
+			joiningEvent: false
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -30,14 +31,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
@@ -45,108 +46,134 @@ const getState = ({ getStore, getActions, setStore }) => {
 			addFavoritePhoto: (photo) => {
 				const store = getStore();
 				const updatedFavorites = store.favorites.some((fav) => fav.index === photo.index)
-				  ? store.favorites.filter((fav) => fav.index !== photo.index)
-				  : [...store.favorites, photo];
-		
+					? store.favorites.filter((fav) => fav.index !== photo.index)
+					: [...store.favorites, photo];
+
 				const updatedCardFavorites = {
-				  ...store.cardFavorites,
-				  [photo.index]: updatedFavorites.filter((fav) => fav.index === photo.index).length,
+					...store.cardFavorites,
+					[photo.index]: updatedFavorites.filter((fav) => fav.index === photo.index).length,
 				};
-		
+
 				setStore({
-				  favorites: updatedFavorites,
-				  cardFavorites: updatedCardFavorites,
+					favorites: updatedFavorites,
+					cardFavorites: updatedCardFavorites,
 				});
-			  },
-		
-			  deleteFavoritePhoto: (index) => {
+			},
+
+			deleteFavoritePhoto: (index) => {
 				const store = getStore();
 				const updatedFavorites = store.favorites.filter((fav, i) => i !== index);
-		
+
 				// Actualiza el recuento de favoritos por tarjeta
 				const updatedCardFavorites = {
-				  ...store.cardFavorites,
-				  [index]: updatedFavorites.length,
+					...store.cardFavorites,
+					[index]: updatedFavorites.length,
 				};
-		
-				setStore({
-				  favorites: updatedFavorites,
-				  cardFavorites: updatedCardFavorites,
-				});
-			  },
 
-			  addLikePhoto: (photo) => {
+				setStore({
+					favorites: updatedFavorites,
+					cardFavorites: updatedCardFavorites,
+				});
+			},
+
+			addLikePhoto: (photo) => {
 				const store = getStore();
 				const updatedLikes = store.likes.some((like) => like.index === photo.index)
-				  ? store.likes.filter((like) => like.index !== photo.index)
-				  : [...store.likes, photo];
-		
+					? store.likes.filter((like) => like.index !== photo.index)
+					: [...store.likes, photo];
+
 				const updatedCardLikes = {
-				  ...store.cardLikes,
-				  [photo.index]: updatedLikes.filter((like) => like.index === photo.index).length,
+					...store.cardLikes,
+					[photo.index]: updatedLikes.filter((like) => like.index === photo.index).length,
 				};
-		
+
 				setStore({
-				  likes: updatedLikes,
-				  cardLikes: updatedCardLikes,
+					likes: updatedLikes,
+					cardLikes: updatedCardLikes,
 				});
-			  },
-			  
+			},
+
 			toggleStatus: () => {
 				const store = getStore();
 				setStore({ statusActive: !store.statusActive });
-			  },
-			
+			},
+
 			setVistaElement: (componentType) => {
 				setStore({ vistaProfile: componentType });
-				},
+			},
 			clearVista: () => {
 				setStore({ vistaProfile: null });
-				},	
-				loginUser: () => setStore({ isUserLoggedIn: true, isAdminLoggedIn: false }),
-				loginAdmin: () => setStore({ isUserLoggedIn: false, isAdminLoggedIn: true }),
-				logout: () => setStore({ isUserLoggedIn: false, isAdminLoggedIn: false }),
+			},
+			loginUser: () => setStore({ isUserLoggedIn: true, isAdminLoggedIn: false }),
+			loginAdmin: () => setStore({ isUserLoggedIn: false, isAdminLoggedIn: true }),
+			logout: () => setStore({ isUserLoggedIn: false, isAdminLoggedIn: false }),
 
 			getEvent: async (event_id) => {
 				try {
 					const url = `${process.env.BACKEND_URL}/events/${event_id}`;
 					const response = await fetch(url);
-				
+
 					if (!response.ok) {
-							throw new Error(`Status: ${response.status}`);
+						throw new Error(`Status: ${response.status}`);
 					}
-				
+
 					const data = await response.json();
-				
+
 					// Verificar si data es un objeto JSON válido antes de actualizar el store
 					if (data && typeof data === 'object') {
 						setStore({ currentEvent: data });
 					} else {
 						console.error('La respuesta no es un objeto JSON válido:', data);
 					}
-					} catch (err) {
-						console.error(`Error al obtener el evento ${event_id}:`, err);
-					}
-				},
-				getAllEvents: () => {
-					fetch(process.env.BACKEND_URL + "/events")
-						.then(response => {
-							if (!response.ok) {
-								throw new Error(`Status: ${response.status}`);
-							}
-							return response.json();
-						})
-						.then(data => {
-							if (data && typeof data === 'object') {
-								setStore({ events: data });
-								console.log(data)
-							} else {
-								console.error('La respuesta no es un array JSON válido:', data);
-							}
-						})
-						.catch(err => {
-							console.error("Error:", err);
-						});
+				} catch (err) {
+					console.error(`Error al obtener el evento ${event_id}:`, err);
 				}
-}}}
+			},
+			getAllEvents: () => {
+				fetch(process.env.BACKEND_URL + "/events")
+					.then(response => {
+						if (!response.ok) {
+							throw new Error(`Status: ${response.status}`);
+						}
+						return response.json();
+					})
+					.then(data => {
+						if (data && typeof data === 'object') {
+							setStore({ events: data });
+							console.log(data)
+						} else {
+							console.error('La respuesta no es un array JSON válido:', data);
+						}
+					})
+					.catch(err => {
+						console.error("Error:", err);
+					});
+
+			},
+			joinEvent: async (event_id) => {
+				try {
+					setStore({ joiningEvent: true })
+					const response = await fetch(`${process.env.BACKEND_URL}/events/${event_id}/join`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${token}`,
+						},
+					})
+					if (response.ok) {
+						console.log("usuario unido al evento con exito")
+					}
+					else {
+						console.error("error al unirse al evento", response.status)
+					}
+				}
+				catch(error){
+					console.error("Error:", error);
+				}
+				finally {
+					setStore({ joiningEvent: false });
+				}
+		}
+	}
+}}
 export default getState;
