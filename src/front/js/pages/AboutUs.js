cadfc2181a967ad7021accaf4c532332b042b7d3
Santiago Quijano <santiquijano77@gmail.com>
2024-02-08 22:36:21 +0000

    Sección About Us terminada

--- src/front/js/pages/AboutUs.js
+++ src/front/js/pages/AboutUs.js
@@ -10,14 +10,14 @@ export const AboutUs = () => {
                     <img src='https://apertura.cl/tienda/modules/ph_simpleblog/featured/19.jpg' alt='placeholder image' className='about-us-img img-fluid rounded-circle mb-4' />
                     <h2 className='display-4 about-us-title'>Nuestro Objetivo</h2>
                     <p className='lead'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget nisl vehicula, auctor massa sed, auctor risus. Donec ac augue leo. In ullamcorper mollis sapien, at ullamcorper elit imperdiet at. Pellentesque pretium metus nec ipsum molestie auctor. Cras eu sem ligula. Nunc aliquam posuere elit, vitae fermentum lectus tempus ac.
+                        Ser el destino definitivo para todos los amantes de la fotografía, desde aficionados entusiastas hasta profesionales consumados. Queremos fomentar una comunidad inclusiva y vibrante donde cada miembro pueda compartir su pasión por la fotografía, encontrar inspiración en el trabajo de otros y mejorar sus habilidades a través de la colaboración y el intercambio de conocimientos.
                     </p>
                 </div>
                 <div className='col-md-6'>
                     <div className='card text-white p-4' style={{ backgroundColor: '#2B2B2B' }}>
                         <h2 className='fw-bold mb-4'>¡Únete a nuestra Comunidad!</h2>
                         <p className='mb-4'>
-                            Fusce pharetra condimentum lorem, egestas venenatis diam consequat nec. Nunc varius risus eu congue rhoncus. Aenean eu rutrum lorem. Aenean ante mauris, aliquam eget lorem vitae, congue dictum dolor. In rhoncus, felis nec viverra feugiat, sapien mi tincidunt metus, eget ultricies nisi ex sed massa. Cras convallis arcu nec fringilla viverra.
+                            Únete a nuestra comunidad de fotógrafos apasionados y encuentra un lugar donde puedas expresar tu creatividad, aprender nuevas técnicas y conectarte con personas que comparten tu amor por la fotografía. Ya seas un principiante emocionado o un profesional experimentado, todos son bienvenidos aquí para compartir su trabajo, recibir retroalimentación constructiva y celebrar juntos la belleza del mundo a través de la lente.
                         </p>
                         <div className='d-flex justify-content-center'>
                             <Link to="/signUp"><button type='button' className='btn about-us-btn text-white rounded-pill px-4 py-2 me-3'>¡Regístrate!</button></Link>
@@ -30,7 +30,7 @@ export const AboutUs = () => {
                     <div className='card about-us-card text-white p-4'>
                         <h2 className='display-6 fw-bold'>Fotógrafos</h2>
                         <p>
-                            Fusce pharetra condimentum lorem, egestas venenatis diam consequat nec. Nunc varius risus eu congue rhoncus. Aenean eu rutrum lorem. Aenean ante mauris, aliquam eget lorem vitae, congue dictum dolor. In rhoncus, felis nec viverra feugiat, sapien mi tincidunt metus, eget ultricies nisi ex sed massa. Cras convallis arcu nec fringilla viverra.
+                            Sumérgete en un mundo de creatividad y descubre nuevas perspectivas a través de la fotografía. Nuestra comunidad está formada por fotógrafos de todos los niveles y estilos, desde los que están empezando hasta los profesionales establecidos. Comparte tus obras, obtén inspiración de otros y haz conexiones significativas en un ambiente de apoyo y aprendizaje mutuo.
                         </p>
                     </div>
                 </div>
@@ -38,7 +38,7 @@ export const AboutUs = () => {
                     <div className='card about-us-card text-white p-4'>
                         <h2 className='display-6 fw-bold'>Dona</h2>
                         <p>
-                            Fusce pharetra condimentum lorem, egestas venenatis diam consequat nec. Nunc varius risus eu congue rhoncus. Aenean eu rutrum lorem. Aenean ante mauris, aliquam eget lorem vitae, congue dictum dolor. In rhoncus, felis nec viverra feugiat, sapien mi tincidunt metus, eget ultricies nisi ex sed massa. Cras convallis arcu nec fringilla viverra.
+                            Tu generosidad nos ayuda a mantener esta comunidad viva y en crecimiento. Cada donación, por pequeña que sea, contribuye a financiar eventos, premios y mejoras en nuestra plataforma para que podamos seguir inspirando y apoyando a la comunidad fotográfica. Únete a nosotros y sé parte de este viaje emocionante para promover la fotografía como forma de expresión y conexión.
                         </p>
                         <div className='d-flex justify-content-center'>
                             <a href='https://www.buymeacoffee.com/snapify'>
@@ -49,9 +49,9 @@ export const AboutUs = () => {
                 </div>
                 <div className='col-md-4'>
                     <div className='card about-us-card text-white p-4'>
-                        <h2 className='display-6 fw-bold'>¡Danos una mano!</h2>
+                        <h2 className='display-6 fw-bold'>¡Colabora con Nosotros!</h2>
                         <p>
-                            Fusce pharetra condimentum lorem, egestas venenatis diam consequat nec. Nunc varius risus eu congue rhoncus. Aenean eu rutrum lorem. Aenean ante mauris, aliquam eget lorem vitae, congue dictum dolor. In rhoncus, felis nec viverra feugiat, sapien mi tincidunt metus, eget ultricies nisi ex sed massa. Cras convallis arcu nec fringilla viverra.
+                            Buscamos personas comprometidas y entusiastas que deseen colaborar con nuestra comunidad fotográfica. Ya sea que puedas dedicar tiempo como voluntario, compartir tus habilidades en talleres o contribuir con recursos, ¡tu colaboración es valiosa y apreciada! Juntos, podemos seguir creando un espacio vibrante y enriquecedor para todos los amantes de la fotografía.
                         </p>
                     </div>
                 </div>
